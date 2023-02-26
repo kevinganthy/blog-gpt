6dc99397d246cc363004be610b4d51cf7cba5480
Kevin Ganthy <kevin.ganthy@gmail.com>
2023-02-26 14:17:52 +0100

    path

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import autoPreprocess from 'svelte-preprocess';
 
 export default defineConfig({
+  base: '',
   plugins: [
     svelte({
 			preprocess: autoPreprocess()
